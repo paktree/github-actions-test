52df36e1ea0f6103e95102a1c82afa73f2cf255b
chung-lai <87332578+chung-lai@users.noreply.github.com>
2022-09-22 15:38:58 -0700

    fix

--- test.rb
+++ test.rb
@@ -1,8 +1,4 @@
 value = `git rev-parse --abbrev-ref HEAD`
 `touch b`
-<<<<<<< Updated upstream
-`git commit -a "hello"`
-=======
 `git commit -a -m "hello"`
->>>>>>> Stashed changes
 `git push origin b`
